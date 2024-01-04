aa709831b1fd47089495f0083ab2aa73ac69bcff
Colin Bagley <cjbagley@googlemail.com>
2024-01-04 20:01:37 +0000

    Added book info report

--- main.py
+++ main.py
@@ -2,15 +2,9 @@ import sys
 
 def main() -> int:
     try:
-        book_filepath = "books/frankenstein.txt"
-        book_content = get_book_content(book_filepath)
-        wc = get_word_count(book_content)
-        print(f"Wordcount for {book_filepath}: {wc}")
-        lc = get_letter_count(book_content)
-        print("Printing letter counts - get ready...")
-        print(lc)
-    except:
-        print("Error occurred")
+        print_book_info_report("books/frankenstein.txt")
+    except Exception as err:
+        print(f"Error occurred: {err}")
     return 0
 
 def get_book_content(filepath) -> str:
@@ -25,11 +19,27 @@ def get_letter_count(text) -> dict:
     counts = {}
     for letter in text:
         letter = letter.lower()
+
         if letter in counts:
             counts[letter] += 1
         else:
             counts[letter] = 1
+
     return counts
+
+def print_book_info_report(book_filepath):
+        book_content = get_book_content(book_filepath)
+        wc = get_word_count(book_content)
+        lc = get_letter_count(book_content)
+        letters = list(lc)
+        letters.sort()
+        print(f"--- Begin report of {book_filepath} ---")
+        print(f"{wc} words found in the document\n")
+        for letter in letters:
+            if not letter.isalpha():
+                continue
+            count = lc[letter]
+            print(f"The '{letter}' character was found {count} times")
 
 if __name__ == '__main__':
     sys.exit(main())
